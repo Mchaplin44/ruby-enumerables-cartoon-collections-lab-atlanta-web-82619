1ba56a7dee9bdcbd71f7c65556ad655dc631ac65
Martin Chaplin <martin.chaplin44@gmail.com>
2019-08-20 23:41:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,16 @@ def summon_captain_planet(array)
   planeteer_calls
 end
 
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
+end
 
-}
 def find_the_cheese(array)
   array.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
